9beb29f30371fbc374b5c188ed91146e41c4be5e
Looyyd <filip.a.niedzielski@gmail.com>
2024-12-25 21:59:06 +0100

    icons and animated CTA button

--- apps/web-frontend/src/app/page.tsx
+++ apps/web-frontend/src/app/page.tsx
@@ -2,26 +2,32 @@
 
 import Link from "next/link";
 import { Visualizer } from "../components/LandingPageVisualizer";
-import { StarIcon, SparklesIcon } from "@heroicons/react/24/solid";
+import {
+  StarIcon,
+  SparklesIcon,
+  LightBulbIcon,
+} from "@heroicons/react/24/solid";
+import { FaWindows } from "react-icons/fa";
+import { AnimatedButton } from "../components/AnimatedButton";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center bg-background text-foreground">
       {/* Hero Section */}
-      <div className="w-full bg-gradient-to-b from-primary/10 to-background py-10">
+      <div className="w-full bg-gradient-to-b from-primary/10 to-background py-8">
         <div className="container flex flex-col items-center justify-center gap-6 px-4">
           <h1 className="brand-text text-5xl font-extrabold tracking-tight leading-tight text-primary text-center sm:text-[5rem]">
             Draftking
           </h1>
           <p className="max-w-2xl text-xl text-center text-muted-foreground">
             The most accurate League of Legends draft tool, powered by AI{" "}
-            <SparklesIcon className="inline-block h-5 w-5 " />
+            <SparklesIcon className="inline-block h-5 w-5 mb-1" />
           </p>
         </div>
       </div>
 
       {/* Main Content */}
-      <div className="container flex flex-col items-center justify-center px-4 py-16 space-y-24">
+      <div className="container flex flex-col items-center justify-center px-4 py-12 space-y-24">
         {/* Why Draftking Section */}
         <section className="flex flex-col items-center max-w-4xl">
           <h2 className="text-3xl font-bold mb-8">
@@ -43,6 +49,11 @@ export default function HomePage() {
                 well as ally champion synergies and anti-synergies, team damage
                 distributions, late vs early game dynamics, and so on.
               </p>
+              <div className="mt-12 w-full flex justify-center">
+                <AnimatedButton href="/draft">
+                  Analyse a Draft Now
+                </AnimatedButton>
+              </div>
             </div>
             <Visualizer />
           </div>
@@ -52,7 +63,8 @@ export default function HomePage() {
         <section className="w-full bg-secondary/10 rounded-2xl p-8">
           <div className="flex flex-col items-center max-w-4xl mx-auto">
             <h2 className="text-3xl font-bold mb-6">
-              Champion recommendations
+              Champion recommendations{" "}
+              <LightBulbIcon className="inline-block h-7 w-7 mb-2" />
             </h2>
             <p className="text-lg text-center">
               <span className="brand-text">Draftking </span> can help you pick
@@ -73,7 +85,10 @@ export default function HomePage() {
 
         {/* Desktop Version Section */}
         <section className="flex flex-col items-center max-w-4xl">
-          <h2 className="text-3xl font-bold mb-6">Desktop version</h2>
+          <h2 className="text-3xl font-bold mb-6">
+            Desktop version{" "}
+            <FaWindows className="inline-block h-7 w-7 mb-1 ml-1" />
+          </h2>
           <p className="text-lg text-center">
             <span className="brand-text">Draftking</span> is also available as a
             Windows desktop application. The desktop application can connect
